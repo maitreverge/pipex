00ecf5403f871d5c04d21b4a554202f8daed4964
de_la_verge <verge.f@gmail.com>
2023-11-19 12:31:46 +0100

    init multiples docs

---
+++ pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 14:41:08 by flverge           #+#    #+#             */
+/*   Updated: 2023/11/18 17:18:30 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h> // write, close, access, dup, dup2
+# include <fcntl.h> // open and options
+# include <stdlib.h> // malloc and free
+# include <stdio.h> // perror
+# include <errno.h> // makes errno a global variable usable with strerror(errno)
+
+#endif
--- understanding.c
+++ understanding.c
@@ -2,23 +2,133 @@
 
 Authorized functions :
 
-open :
+! open : open a file descriptor with a read / write function
 
-close :
+! close : close the fd
 
-read :
+! read : read(fd, original_buffer, BUFFER_SIZE);
 
-Write :
+! Write : => ft_putchar maggle
 
-Malloc :
+! Malloc : tu coco
 
-Free :
+! Free : pareil
 
-perror:
+! perror: void perror(const char *str);  
 
-strerror :
 
-access :
+The perror function in C is used to print a description for the last error that occurred during a system call or library function. It's particularly helpful when you want to provide more informative error messages than just the error code. Here's a brief explanation:
+
+Function: void perror(const char *s);
+
+Description: Prints a description for the last error that occurred during a system call or library function.
+
+Parameters:
+
+s: A pointer to a string that will be printed before the error message. It's typically used to provide additional context to the error.
+
+
+
+! strerror :
+
+strerror is another function in C that serves a similar purpose to perror.
+It's used to retrieve a pointer to the textual representation of the current errno value (error number).
+
+Here's a breakdown:
+Function: char *strerror(int errnum);
+
+Description: Returns a pointer to the textual representation of the current errno value (errnum).
+
+Parameters:
+
+errnum: The error number for which you want the error message.
+
+EXAMPLE :
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main() {
+    FILE *file = fopen("nonexistent_file.txt", "r");
+
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file: %s\n", strerror(errno));
+        // Or you can use perror directly: perror("Error opening file");
+        // perror adds the error message automatically, while strerror requires explicit format
+        // specifier in the printf function.
+        
+        exit(EXIT_FAILURE);
+    }
+
+    // Rest of the code...
+
+    return 0;
+}
+
+/*
+
+
+! access :
+
+The access function in C is used to check the accessibility of a file or a path based on the permissions specified.
+It can be used to check whether a file can be read, written, or executed by the current process. Here's an overview:
+
+Function: int access(const char *path, int mode);
+
+Description: Checks whether the calling process can access the file specified by the path.
+
+Parameters:
+
+path: The path to the file or directory you want to check.
+mode: The mode of access you want to check (read, write, execute, or a combination).
+Return Value:
+
+Returns 0 if the access is granted according to the specified mode.
+Returns -1 if an error occurs, and errno is set to indicate the specific error.
+
+Examples :
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main() {
+    const char *file_path = "example.txt";
+
+    // Check if the file is readable
+    if (access(file_path, R_OK) == 0) {
+        printf("File is readable.\n");
+    } else {
+        perror("Error checking file accessibility");
+        exit(EXIT_FAILURE);
+    }
+
+    // Check if the file is writable
+    if (access(file_path, W_OK) == 0) {
+        printf("File is writable.\n");
+    } else {
+        perror("Error checking file accessibility");
+        exit(EXIT_FAILURE);
+    }
+
+    // Check if the file is executable
+    if (access(file_path, X_OK) == 0) {
+        printf("File is executable.\n");
+    } else {
+        perror("Error checking file accessibility");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+/*
 
 dup :
 
