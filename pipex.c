09ada04e3508042c9b339503232948d84f079c54
de_la_verge <verge.f@gmail.com>
2023-11-22 21:38:42 +0100

    Ok g kompri 1 truc je kroi

--- pipex.c
+++ pipex.c
@@ -6,19 +6,45 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:12:36 by flverge           #+#    #+#             */
-/*   Updated: 2023/11/19 13:38:31 by flverge          ###   ########.fr       */
+/*   Updated: 2023/11/22 21:38:13 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int ac, char **av)
+void	pipex_madatory(void)
 {
-	if (ac >= 5)
+	// mdr je met quoi ici ?
+}
+
+void	pipex_bonus(void)
+{
+	// Ok Google, code pipex stp c tro dur :(
+}
+
+int	main(int ac, char **av, char **envp) // ! travailler sur les variables d'environement
+{
+	if (ac >= 4)
 	{
-		// execute pipex mdr     
+		if (ac == 4)
+			pipex_mandatory();
+		else
+			pipex_bonus(); 
 	}
 	else
 	{
 		ft_printf("Not enough arguments.\nUsage : ./pipex  <file1>  <command_1>  <command_2>  ...  <file2>");
 		return (-1);
 	}
+}
+
+// Chat GPT le sang
+#include <stdio.h>
+
+int main(int argc, char **argv, char **envp) {
+    // Print environment variables
+    while (*envp != NULL) {
+        printf("envp: %s\n", *envp);
+        envp++;
+    }
+
+    return 0;
 }
