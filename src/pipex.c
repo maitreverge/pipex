7a3caf1d40cd781d4da00b29839a5ef499828c45
de_la_verge <verge.f@gmail.com>
2023-11-30 11:10:13 +0100

    starting reflexion

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:12:36 by flverge           #+#    #+#             */
-/*   Updated: 2023/11/30 10:34:35 by flverge          ###   ########.fr       */
+/*   Updated: 2023/11/30 11:09:29 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,36 @@
 
 void	pipex_madatory(void)
 {
-	
-	
-}
-
-void	pipex_bonus(void)
-{
-	if (!ft_strcmp(av[2], "here_doc"))
-	{
-		// gerer le here_doc
-	}
-	else // multiple pipes handeling
+	if (!ft_strcmp(av[2], "here_doc")) // gestion bonus 2
 	{
 		
 	}
-	
-	
+	else // mandatory part
+	{
+		// gerer l'ouverture des fd
+		// gerer les droits
+		// gerer les pipes
+		// gerer les forks
+		// gerer les execve
+		// gerer les waitpid
+		// gerer les close
+		// gerer les free
+	}
 }
 
-int	main(int ac, char **av, char **envp) // ! travailler sur les variables d'environement
+int	main(int ac, char **av, char **envp)
 {
 	if (ac >= 4)
 	{
 		if (ac == 4)
 			pipex_mandatory(ac, av, envp);
 		else
-			pipex_bonus(ac, av, envp); 
+			pipex_bonus(ac, av, envp);
+		return (0);
 	}
 	else
 	{
 		ft_printf("Not enough arguments.\nUsage : ./pipex  <file1>  <command_1>  <command_2>  ...  <file2>");
-		return (-1);
+		return (1);
 	}
-}
-
-// Chat GPT le sang
-#include <stdio.h>
-
-int main(int argc, char **argv, char **envp) {
-    // Print environment variables
-    while (*envp != NULL) {
-        printf("envp: %s\n", *envp);
-        envp++;
-    }
-
-    return 0;
 }
--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -6,9 +6,21 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 10:53:42 by flverge           #+#    #+#             */
-/*   Updated: 2023/11/30 10:53:47 by flverge          ###   ########.fr       */
+/*   Updated: 2023/11/30 11:10:01 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
+void	pipex_bonus(void)
+{
+	// gerer l'ouverture des fd
+	// gerer les droits
+	// gerer les pipes
+	// gerer les forks
+	// gerer les execve
+	// gerer les waitpid
+	// gerer les close
+	// gerer les free
+	
+}
