b383a0b3e7c47e2612d8831ca45fb1d8504ac2be
de_la_verge <verge.f@gmail.com>
2023-12-15 11:43:47 +0100

    normed path parser

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 13:00:02 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/15 11:39:20 by flverge          ###   ########.fr       */
+/*   Updated: 2023/12/15 11:43:32 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,28 @@
 
 char	**path_parsing(char **av, char **envp)
 {
-	char **env;
-	char **result;
-	char *path;
+	char	**env;
+	char	**result;
+	char	*path;
 
 	env = envp;
-	while (*env != NULL) // Loop through envp array
+	while (*env != NULL)
 	{
-		if (ft_strncmp(*env, "PATH=", 5) == 0) // Check if the string starts with "PATH="
-			break;
+		if (ft_strncmp(*env, "PATH=", 5) == 0)
+			break ;
 		env++;
 	}
-	// printf("Untrimed string = %s\n\n", *env);
-	if (*env) // Check if PATH has been found, if not exit
+	if (*env)
 	{
 		path = ft_strtrim(*env, "PATH=");
 		if (!path)
-			error_quit("Strtrim path failed");
+			error_quit("Strtrim function failed");
 	}
 	else
 		error_quit("PATH envp couldn't be found");
-	// ! checked trimed string
-	// printf("Strimmed string = %s\n\n", path);
-	
 	result = ft_split(path, ':');
 	if (!result)
-		error_quit("Split path failed");
-
-	// Testing the path splitting
-	// for (int i = 0; result[i] != NULL; i++)
-	// 	printf("Path number %i = %s\n", i + 1, result[i]);
-	// ft_printf("\n\n-------\n\nend parsing path");	
-	
+		error_quit("Split function failed");
 	free(path);
-	return (result); // Needs freeing
+	return (result);
 }
-// ! parser 
-/*
-
-String a parser :
-
-PATH=/nfs/homes/flverge/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-
-parser les variables d'env pour :
-
-1 - retrouver PATH 
-
-2 - Spliter cette string de l'enfer avec le char :
-
-3 - et plus tard, join le string du path avec celui de l'argument (mdr anvi 2 crever)
-
-*/
