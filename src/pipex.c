daaf59bcd69f0a4413c98a62c731a6723ca713e8
de_la_verge <verge.f@gmail.com>
2023-12-24 23:29:57 +0100

    removed unecessary closing fd

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: flverge <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:12:36 by flverge           #+#    #+#             */
-/*   Updated: 2023/12/24 23:19:38 by flverge          ###   ########.fr       */
+/*   Updated: 2023/12/24 23:29:19 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,17 @@ void	ft_execve(t_vars *vars, int arg, int i)
 
 void	child_process(t_vars *vars)
 {
-	dup2(vars->fd[0], STDIN_FILENO);
 	dup2(vars->pipe_fd[1], STDOUT_FILENO);
-	close(vars->fd[0]);
+	dup2(vars->fd[0], STDIN_FILENO);
 	close(vars->pipe_fd[0]);
 	ft_execve(vars, 0, 0);
 }
 
 void	parent_process(t_vars *vars)
 {
-	waitpid(vars->pid, &vars->status, 0);
 	dup2(vars->pipe_fd[0], STDIN_FILENO);
-	close(vars->pipe_fd[0]);
-	close(vars->pipe_fd[1]);
 	dup2(vars->fd[1], STDOUT_FILENO);
-	close(vars->fd[1]);
+	close(vars->pipe_fd[1]);
 	ft_execve(vars, 1, 0);
 }
 
@@ -65,8 +61,8 @@ void	pipex(char **av, t_vars *vars)
 		error_quit("Forking failure");
 	else if (vars->pid == 0)
 		child_process(vars);
-	else
-		parent_process(vars);
+	waitpid(vars->pid, &vars->status, 0);
+	parent_process(vars);
 }
 
 int	main(int ac, char **av, char **envp)
