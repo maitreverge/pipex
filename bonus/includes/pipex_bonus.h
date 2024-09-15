40a0aa39edc1547ab2b1fc2c1fc3c85c168c3b48
de_la_verge <verge.f@gmail.com>
2024-09-15 12:55:44 +0200

    Refactor Arborecense

---
+++ bonus/includes/pipex_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 12:33:02 by flverge           #+#    #+#             */
+/*   Updated: 2024/09/15 12:33:11 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H 
+
+# include "../libft/libft/libft.h"
+# include "../libft/ft_printf/ft_printf.h"
+# include "../libft/get_next_line/get_next_line_bonus.h"
+# include <unistd.h> // write, close, access, dup, dup2, execve
+# include <fcntl.h> // open and options
+# include <stdlib.h> // malloc and free and EXIT_SUCCESS / EXIT_FAILURE
+# include <stdio.h> // perror
+# include <errno.h> // makes errno a global variable usable with strerror(errno)
+# include <string.h>
+# include <sys/wait.h>
+
+
+
+#endif
