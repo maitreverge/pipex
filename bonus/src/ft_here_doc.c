c6555c28d90a19a5891c661e2a6eee4d4ee838c8
“florianverge” <verge.f@gmail.com>
2024-09-30 18:48:29 +0200

    `here_doc` working on buffer, still leaking

--- bonus/src/ft_here_doc.c
+++ bonus/src/ft_here_doc.c
@@ -6,11 +6,38 @@
 /*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:08:15 by flverge           #+#    #+#             */
-/*   Updated: 2024/09/29 12:57:06 by flverge          ###   ########.fr       */
+/*   Updated: 2024/09/30 18:47:59 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+
+int	custom_get_next_line(char **line)
+{
+	char	*buffer;
+	int		i;
+	int		read_buff;
+	char	c;
+
+	i = 0;
+	read_buff = 0;
+	buffer = (char *)malloc(10000);
+	if (!buffer)
+		return (-1);
+	read_buff = read(0, &c, 1);
+	while (read_buff && c != '\n' && c != '\0')
+	{
+		if (c != '\n' && c != '\0')
+			buffer[i] = c;
+		i++;
+		read_buff = read(0, &c, 1);
+	}
+	buffer[i] = '\n';
+	buffer[++i] = '\0';
+	*line = buffer;
+	// free(buffer);
+	return (read_buff);
+}
 
 void	failed_io(char *buffer, int fd_here_doc, char *message)
 {
@@ -23,92 +50,55 @@ void	failed_io(char *buffer, int fd_here_doc, char *message)
 // 		cmd << LIMITER | cmd1 >> file
 void	ft_here_doc(char **av, char **envp, t_paths **paths)
 {
-	// (void)(envp);
-	int fd_here_doc;
-	char *here_doc_file_name;
-	char *buffer;
-	ssize_t bytes_read;
-	int fd[2];
-
-	if (pipe(fd) == -1)
-		exit_and_message("Pipe failed", 1);
-	
-	
-	// int is_heredoc_exists;
-
-	// TODO : Free eof
-	char *eof = ft_strjoin(av[1], "\n\0");
-	
-	// ! gestion erreur malloc
-	buffer = malloc(sizeof(char) * 1024);
-
-	// a file random of size 10 is 35^10 possibilities
-	here_doc_file_name = "INPUT_FILE";
-	// here_doc_file_name = generate_random_string(10);
-
-	int fork_pid = fork();
-	
-	if (fork_pid == 0)
-	{
-
-		
-		fd_here_doc = open(here_doc_file_name, O_RDWR | O_CREAT | O_TRUNC, 0664);
-		if (fd_here_doc == -1)
+    int fd[2];
+    char *main_buf;
+    int fork_pid;
+    char* eof = av[1];
+    
+    if (pipe(fd) == -1)
+        exit_and_message("Pipe failed", 1);
+    fork_pid = fork();
+    
+    if (fork_pid == 0) // child
+    {
+        // child process
+        close(fd[0]);
+		while (custom_get_next_line(&main_buf))
 		{
-			free_paths(*paths);
-			exit_and_message("Input file can't be openned\nAborting pipex", 1);
+			if (ft_strncmp(main_buf, eof, ft_strlen(eof)) == 0)
+			{
+				free(main_buf);
+				exit(0);
+			}
+			write(fd[1], main_buf, ft_strlen(main_buf));
+			free(main_buf);
 		}
-
-
-		// reading and writting in the file
-		while(true)
-		{
-			bytes_read = read(STDIN_FILENO, buffer, 1024 -1); // exclude last for '\0'
-			if (bytes_read == -1)
-				failed_io(buffer, fd_here_doc, "Error reading from STDIN");
-			
-			buffer[bytes_read] = '\0'; // append terminating char
-			
-			if (!ft_strcmp(buffer, eof)) // match from the EOF
-				break;
-			
-			if (write(fd_here_doc, buffer, bytes_read) == -1)
-				failed_io(buffer, fd_here_doc, "Error writing to here_doc file");
-		}
-
-		dup2(fd_here_doc, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd_here_doc);
-		ft_exec(av[2], envp, paths);
-		exit(0);
-		// unlink(here_doc_file_name);
-		
-	} // end child process
-
-	waitpid(fork_pid, NULL, 0);
-	
-	/*
-	! ./pipex_bonus  here_doc LIMITER cmd cmd1 file
-	*/
-	// remove file_in
-		
-	// Parent process
-	
-	char	*output_file;
-	int		fd_output_file;
-
-	output_file = av[4]; // last arg for 
-	fd_output_file = open(output_file, O_RDWR | O_CREAT | O_APPEND, 0664);
-	if (fd_output_file == -1)
-	{
-		free_paths(*paths);
-		exit_and_message("Output file can't be openned\nAborting pipex", 1);
 	}
-	dup2(fd[0], STDIN_FILENO);
-	dup2(fd_output_file, STDOUT_FILENO);
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[1]);
-	close(fd_output_file);
-	ft_exec(av[3], envp, paths);
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		wait(NULL);
+	}
+    
+    waitpid(fork_pid, 0, 0);
+
+    // Parent process
+    char	*output_file;
+    int		fd_output_file;
+
+    output_file = av[4]; // last arg for 
+    fd_output_file = open(output_file, O_RDWR | O_CREAT | O_APPEND, 0664);
+    if (fd_output_file == -1)
+    {
+        free_paths(*paths);
+        exit_and_message("Output file can't be openned\nAborting pipex", 1);
+    }
+    dup2(fd_output_file, STDOUT_FILENO);
+    dup2(fd[0], STDIN_FILENO);
+    close(fd[1]);
+    close(fd_output_file);
+    ft_exec(av[3], envp, paths);
+
+    // Cleanup
 }
