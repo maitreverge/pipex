8ddabd4bea6152499553f0c0fc4263df450a2931
“maitreverge_42” <verge.f@gmail.com>
2024-12-17 14:43:14 +0100

    wip pipex

--- bonus/src/ft_multi_pipes.c
+++ bonus/src/ft_multi_pipes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_multi_pipes.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flverge <flverge@student.42.fr>            +#+  +:+       +#+        */
+/*   By: TryHardTeam <TryHardTeam@123.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:08:28 by flverge           #+#    #+#             */
-/*   Updated: 2024/10/06 11:07:38 by flverge          ###   ########.fr       */
+/*   Updated: 2024/12/17 14:43:00 by TryHardTeam      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,27 @@ static void	child_process_mp(char **av, char **envp, t_paths **paths, int *fd)
 	close(fd[0]);
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[1]);
-	ft_exec(av[0], envp, paths);
+	// ft_exec(av[0], envp, paths);
 }
 
-static void	parent_process_mp(char **av, char **envp, t_paths **paths, int *fd)
+void	ft_multi_pipes(char **av, int ac, char **envp, t_paths **paths)
 {
+    int	fd[2];
+    int	fork_pid;
+    char	*input_file;
+    int		fd_file_in;
+    int		fd_file_out;
+    int		i = 0;
+
+	// Open in file
+    input_file = av[0];
+    fd_file_in = open(input_file, O_RDONLY);
+    if (fd_file_in == -1)
+    {
+        free_paths(*paths);
+        exit_and_message("Input file can't be opened\nAborting pipex", 1);
+    }
+	 // open outfile
 	char	*output_file;
 	int		fd_file;
 
@@ -32,31 +48,8 @@ static void	parent_process_mp(char **av, char **envp, t_paths **paths, int *fd)
 	    free_paths(*paths);
 	    exit_and_message("Output file can't be opened\nAborting pipex", 1);
 	}
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[0]);
-	close(fd[1]);
-	dup2(fd_file, STDOUT_FILENO);
-	close(fd_file);
-	ft_exec(av[0], envp, paths); // ! FIRST ARGUMENT AFTER LOOPING
-}
-
-void	ft_multi_pipes(char **av, int ac, char **envp, t_paths **paths)
-{
-    int	fd[2];
-    int	fork_pid;
-    char	*input_file;
-    int		fd_file;
-    int		i = 0;
-
-    input_file = av[0];
-    fd_file = open(input_file, O_RDONLY);
-    if (fd_file == -1)
-    {
-        free_paths(*paths);
-        exit_and_message("Input file can't be opened\nAborting pipex", 1);
-    }
-    dup2(fd_file, STDIN_FILENO);
-    close(fd_file);
+    dup2(fd_file_in, STDIN_FILENO);
+    // close(fd_file);
 
     while (i++ < ac - 4)
     {
@@ -72,11 +65,14 @@ void	ft_multi_pipes(char **av, int ac, char **envp, t_paths **paths)
         }
         else
         {
-            waitpid(fork_pid, 0, 0);
             close(fd[1]); // Close the write end of the pipe in the parent
             dup2(fd[0], STDIN_FILENO); // Redirect stdin to the read end of the pipe for the next command
-            close(fd[0]); // Close the read end of the pipe after duplicating
+            waitpid(fork_pid, 0, 0);
+            // close(fd[0]); // Close the read end of the pipe after duplicating
         }
     }
-    parent_process_mp(++av, envp, paths, fd);
+	dup2(fd_file_out, STDOUT_FILENO);
+	ft_exec(av[ac - 2], envp, paths);
+
+    // parent_process_mp(++av, envp, paths, fd);
 }
